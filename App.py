8f8a449fda4e392f285c0a3c514ec0e966eab357
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2023-07-12 09:00:20 +0200

    fix: minor changes

--- App.py
+++ App.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
+#!/usr/bin/env python
+from fastapi import FastAPI, Response, status
 from dotenv import load_dotenv
 from Models import Student
 import databases
 import os
 
-load_dotenv()
+load_dotenv(override=True)
 
 app = FastAPI()
 database = databases.Database(os.environ["MYSQL_ADDON_URI"])
+print(os.environ["MYSQL_ADDON_URI"])
 
 
 @app.on_event("startup")
@@ -20,8 +22,19 @@ async def shutdown():
     await database.disconnect()
 
 
-@app.post("/students")
+@app.post("/students", tags=["Create", "Students"], summary="Create a new student")
 async def create_student(student: Student):
+    """
+    This endpoint allows you to create a new student in the database.
+
+    - **id**: DNI of the student (mandatory)
+    - **first_name**: First name of the student (mandatory)
+    - **last_name**: Last name of the student (mandatory)
+    - **phone**: Phone number of the student (mandatory)
+    - **email**: Email of the student (mandatory)
+    - **age**: Age of the student (mandatory)
+    - **id_familiar**: DNI of the familiar (optional)
+    """
     query = f"INSERT INTO students (id_students, first_name, last_name, phone, email, age, id_familiar) VALUES (:id_students, :first_name, :last_name, :phone, :email, :age, :id_familiar)"
     values = {
         "id_students": student.id_students,
@@ -37,8 +50,38 @@ async def create_student(student: Student):
     return {"message": "Student created successfully"}
 
 
-@app.get("/")
+@app.get(
+    "/",
+    tags=["Read", "Students"],
+    summary="Get all students",
+    response_description="All students in database shown",
+)
 async def first_api():
+    """
+    This endpoint allows you to get all students in the database.
+    """
     query = "SELECT * FROM students"
     results = await database.fetch_all(query)
-    return {"message": "Hello World", "data": results}
+    return {"message": "All students", "data": results}
+
+
+@app.get(
+    "/students/{id_students}",
+    status_code=status.HTTP_200_OK,
+    tags=["Read", "Students"],
+    summary="Get a student by id",
+)
+async def get_student(id_students: int, response: Response):
+    """
+    This endpoint allows you to get a student by id.
+
+    - **id**: DNI of the student (mandatory)
+    """
+    query = f"SELECT * FROM students WHERE id_students = {id_students}"
+    results = await database.fetch_all(query)
+    if len(results) == 0:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return f"Student with id: {id_students} not found"
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {"message": "Student found", "data": results[0]}
