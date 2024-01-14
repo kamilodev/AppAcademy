36b7dd867cd7df7962a85cbf0f2f38814e14f472
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2024-01-14 19:52:32 +0100

    fix: Connection to DB

--- App.py
+++ App.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 from data.connection import database as database
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import RedirectResponse
-
 from routers import (
     students,
     professors,
@@ -30,21 +27,12 @@ routers = [
 for router in routers:
     app.include_router(router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
+
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
-@app.get("/")
-def redirect_to_docs():
-    return RedirectResponse(url="/docs", status_code=302)
