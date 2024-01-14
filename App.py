167a3a6f627d7014104edf6014c68cf38c1ad53d
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2024-01-15 00:33:49 +0100

    fix: Change MySQL URI and database script

--- App.py
+++ App.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from data.connection import database as database
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+
 from routers import (
     students,
     professors,
@@ -27,12 +30,25 @@ routers = [
 for router in routers:
     app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
-
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+@app.get("/")
+def redirect_to_docs():
+    return RedirectResponse(url="/docs", status_code=302)
+
+@app.get("/{path:path}", include_in_schema=False)
+def redirect_invalid_path(path: str):
+    return RedirectResponse(url="/docs", status_code=302)
